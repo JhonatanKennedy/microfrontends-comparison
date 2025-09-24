ef801798eef3e873499b6a6e49c657c4a1207446
Jhonatan Kennedy <jhonatankennedy@live.com>
2025-09-23 21:49:05 -0300

    Add single spa skeleton

---
+++ mf-single-spa/home-app/webpack.config.js
@@ -0,0 +1,19 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "single-spa",
+    projectName: "home-app",
+    webpackConfigEnv,
+    argv,
+    outputSystemJS: false,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    output: {
+      publicPath: "http://localhost:8001/",
+    },
+  });
+};
